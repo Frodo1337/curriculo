fd136eeec4a085d3a74bbd432c6571f9d8b4637b
frodo1337 <siqueirawm@hotmail.com>
2020-08-11 14:12:06 -0300

    Updates

---
+++ precache-manifest.754d4b4403aab296efa63e1e20e32dc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ddd224f9a2bcb55b2c27d6afa64a43",
+    "url": "/curriculo/index.html"
+  },
+  {
+    "revision": "c6060c1a0dbcc00c2009",
+    "url": "/curriculo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4b3c19ea27778919586f",
+    "url": "/curriculo/static/js/2.62cac21a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/curriculo/static/js/2.62cac21a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6060c1a0dbcc00c2009",
+    "url": "/curriculo/static/js/main.7e2ee103.chunk.js"
+  },
+  {
+    "revision": "e006e6dee268a61f0a42",
+    "url": "/curriculo/static/js/runtime-main.b2059e9d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/curriculo/static/media/logo.5d5d9eef.svg"
+  }
+]);
