6dccd234d4909bd68809cf6db630d266a647151c
frodo1337 <siqueirawm@hotmail.com>
2020-08-11 18:42:02 -0300

    Updates

---
+++ precache-manifest.f5f802cb4474df4a4669ed08119053ac.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "706252e8860560ad40fe151682f1ee43",
+    "url": "/curriculo/index.html"
+  },
+  {
+    "revision": "93da852f7ebb82b3883d",
+    "url": "/curriculo/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "bbc4b1a4224fdc11db38",
+    "url": "/curriculo/static/css/main.e50ae996.chunk.css"
+  },
+  {
+    "revision": "93da852f7ebb82b3883d",
+    "url": "/curriculo/static/js/2.80485b8c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/curriculo/static/js/2.80485b8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbc4b1a4224fdc11db38",
+    "url": "/curriculo/static/js/main.dcb83269.chunk.js"
+  },
+  {
+    "revision": "e006e6dee268a61f0a42",
+    "url": "/curriculo/static/js/runtime-main.b2059e9d.js"
+  },
+  {
+    "revision": "aa1e61dad0a31deba6b118583bcbfbbd",
+    "url": "/curriculo/static/media/bandeira-brasil.aa1e61da.svg"
+  },
+  {
+    "revision": "4e06c50655a273faea7e707cc63c4c55",
+    "url": "/curriculo/static/media/bandeira-estados-unidos.4e06c506.png"
+  },
+  {
+    "revision": "b53ff55e09a092f9f0cbbf74af01b715",
+    "url": "/curriculo/static/media/githubIcon.b53ff55e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/curriculo/precache-manifest.754d4b4403aab296efa63e1e20e32dc5.js"
+  "/curriculo/precache-manifest.f5f802cb4474df4a4669ed08119053ac.js"
 );
 
 self.addEventListener('message', (event) => {
